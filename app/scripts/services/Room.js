5fb23ba297050c0c95d4b211bbe439a1c7fada55
Mark Strizalkouski <markstrizalkouski@gmail.com>
2017-12-04 19:51:33 -0500

    removed console log from Room service

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,7 +3,6 @@
         var Room = {};
         var ref = firebase.database().ref().child("Rooms");
         var rooms = $firebaseArray(ref);
-        console.log(rooms);
         Room.all = rooms;
         return Room;
         
